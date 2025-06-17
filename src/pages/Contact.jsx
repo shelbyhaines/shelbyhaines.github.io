c17cac9995dd75c63f5705a1d4745e2a63ec0b33
Shelby Haines <srh11@sfu.ca>
2025-06-17 14:55:54 -0700

    feat:[#5] Add hyperlinks to contact page
    Added two new classes in index.css (a button class and a class for the boxes). Created 6 cards (boxes) to hold both information and a button linking to external sites. Used a grid class to help structure the boxes.
    
    Included hyperlinks for each of the six buttons, linking to external sites. Included a title related to each link in each box.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,13 +5,106 @@ const Contact = () => {
       <section className="interest-box">
         <div className="introduction-section">
         <h1 className="heading-1 text-center mb-4">
-            Contact Me
+            Contact Me & Learn More
         </h1>
         <p className="main-info-text">
           description here
         </p>
         </div>
       </section>
+
+      {/* content, six sections, each with a link */}
+        <section className="section-grid">
+
+        {/* LinkedIn */}
+        <div className="contact-box h-60 flex flex-col justify-between">
+            <h2 className="small-information-titles">
+                LinkedIn
+            </h2>
+            <p className="main-info-text">
+                description.
+            </p>
+            <a 
+            href="https://www.linkedin.com/in/shelbyhaines?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            >
+            <button className="button">Click here</button>
+            </a>
+        </div>
+
+        {/* resume */}
+        <div className="contact-box h-60 flex flex-col justify-between">
+            <h2 className="small-information-titles">
+                Resume
+            </h2>
+            <p className="main-info-text">
+                Description
+            </p>
+            <a href="https://drive.google.com/file/d/154yWXtHmy7GVqVQre01ETTO9LyOrD3W4/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <button className="button">Click here</button>
+            </a>
+        </div>
+
+        {/* GitHub */}
+        <div className="contact-box h-60 flex flex-col justify-between">
+            <h2 className="small-information-titles">
+                GitHub
+            </h2>
+            <p className="main-info-text">
+                Descripton
+            </p>
+            <a href="https://github.com/shelbyhaines" target="_blank" rel="noopener noreferrer">
+            <button className="button">Click here</button>
+            </a>
+        </div>
+
+        {/* SFU website */}
+        <div className="contact-box h-60 flex flex-col justify-between">
+            <h2 className="small-information-titles">
+                SFU Website
+            </h2>
+            <p className="main-info-text">
+                Description
+            </p>
+            <a href="https://www.sfu.ca/students/calendar/2025/summer/programs/information-systems-in-business-administration-and-computing-science/joint-major/bachelor-of-business-administration-or-bachelor-of-science.html" 
+            target="_blank" rel="noopener noreferrer">
+            <button className="button">Click here</button>
+            </a>
+        </div>
+
+        {/* Email */}
+        <div className="contact-box h-60 flex flex-col justify-between">
+            <h2 className="small-information-titles">
+                Volunteer Roles
+            </h2>
+            <p className="main-info-text">
+                Description
+            </p>
+            <a href="https://young-sprout-117.notion.site/Volunteering-Programs-2155b60ba1ea808aadf4eb594435cf6c?source=copy_link"
+            target="_blank" rel="noopener noreferrer">
+            <button className="button">Click here</button>
+            </a>
+        </div>
+
+        {/* message */}
+        <div className="contact-box h-60 flex flex-col justify-between">
+            <h2 className="small-information-titles">
+                Send a Message
+            </h2>
+            <p className="main-info-text">
+                Description.
+            </p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfeweuIa8NEUOw28673rfBuhz1O9gwwGg9cyjV_qBd2Kt_-ow/viewform?usp=dialog"
+            target="_blank" rel="noopener noreferrer">
+            <button className="button">Click here</button>
+            </a>
+        </div>
+        </section>
+
+      
+
+
     </div>
   );
 };
