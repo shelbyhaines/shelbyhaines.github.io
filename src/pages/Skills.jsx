292058c1d0eb87f7c7b3b62b691fd4fe47e6c4a5
Shelby Haines <srh11@sfu.ca>
2025-06-18 13:08:29 -0700

    feat:[#3] Add the skills page layout
    Created the Skill.jsx file in the pages folder. Added the skills page in the App.jsx file by importing it then adding a new route for skills. Modified Home.jsx to link the skills page on the homepage.
    
    Added the basic structure of the skills page in the Skills.jsx file. This includes the background and the introduction section.

---
+++ src/pages/Skills.jsx
@@ -0,0 +1,19 @@
+function Skills() {
+  return (
+    <div className="page-background-gradients">
+        {/* intro to page */}
+      <section className="interest-box">
+        <div className="introduction-section">
+        <h1 className="heading-1 text-center mb-4">
+            Skills
+        </h1>
+        <p className="main-info-text">
+          description of page
+        </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Skills;
