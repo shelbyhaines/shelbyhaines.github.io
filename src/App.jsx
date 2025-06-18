3900a2d238396183f630da35afb7de61125b9cf4
Shelby Haines <srh11@sfu.ca>
2025-06-17 21:04:43 -0700

    feat:[#7] Add header to all pages
    Add the header in the App.jsx file so that it will appear on all pages. Imported the header path at the top of App.jsx so it could be used, and updated the main code to include the header.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Interests from "./pages/Interests";
 import Contact from "./pages/Contact";
+import Header from './components/Header';
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/interests" element={<Interests />} />
